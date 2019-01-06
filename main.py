73739956d082be3ecb5a1d6dc6b48d50ecd97da3
Levindo Neto <levindogtn@gmail.com>
2019-01-06 14:09:30 -0200

    Add saving specific column in a csv file

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
 dataIn  = "tests/in/medicines.csv"
-
-dataOut = open("tests/out/out.txt", "w")
+dataOut = "tests/out/out.txt"
 
 columnName = 5 #'TP_PRODUTO'
 
+# Create dataframes
 parsedFile = pd.read_csv(dataIn, sep=';', header=None, encoding='utf-8', low_memory=False)
 
+# Get specific column
 savedColumn = parsedFile[columnName]
 
-print(savedColumn)
+# Put column into a csv
+savedColumn.to_csv(dataOut, sep='\t')
